fe572f67b02cb46cec42ac31f3a8179c571e63b0
mounika <mounikapakalapati09@gmail.com>
2024-05-09 16:04:52 -0500

    my name in unicode

---
+++ src/datatypes/Unicodemyname.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+public class Unicodemyname {
+    public static void main(String[] args) {
+        char c1 = '\u0C2E';
+        char c2 = '\u0C4C';
+        char c3 = '\u0C28';
+        char c4 = '\u0C3F';
+        char c5 = '\u0C15';
+        System.out.println(c1+""+c2+""+c3+""+c4+""+c5);
+    }
+}
