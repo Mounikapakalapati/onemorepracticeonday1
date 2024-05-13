616b7fd47691baa08ccbfb5a344400ebc73b9ea1
mounika <mounikapakalapati09@gmail.com>
2024-05-13 18:40:34 -0500

    Infinite loop

---
+++ src/Loopassignments/Infiniteloop.java
@@ -0,0 +1,29 @@
+package Loopassignments;
+
+public class Infiniteloop {
+    public static void main(String[] args) {
+        int i = 0;
+        int j=1;
+        while (true) {
+            i++;
+            System.out.println(i);
+
+        }
+/*
+
+        for (; ; ) {
+            System.out.println(j);
+        }
+
+
+        do{
+            System.out.println(i);
+        }
+        while (true);
+
+*/
+
+    }
+
+    }
+
