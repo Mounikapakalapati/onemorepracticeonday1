2b6ec01fee59d7ee85bedf849b45347ea751692c
mounika <mounikapakalapati09@gmail.com>
2024-05-14 17:54:11 -0500

    Making 17 number with operators

---
+++ src/Loopassignments/Making17withoperators.java
@@ -0,0 +1,13 @@
+package Loopassignments;
+
+public class Making17withoperators {
+    public static void main(String[] args) {
+        int num1 =1;
+        int num2 =2;
+        int num3 =6;
+        int num4 =7;
+
+        int i = ((num3-num1)*num2)+num4;
+        System.out.println(i);
+    }
+}
