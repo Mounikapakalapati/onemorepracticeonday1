281a0760a6050d1aa4b73129da2ae3ffcd42bf63
mounika <mounikapakalapati09@gmail.com>
2024-05-14 18:18:15 -0500

    Nested for loop

---
+++ src/Loopassignments/Nestedforloop.java
@@ -0,0 +1,23 @@
+package Loopassignments;
+
+public class Nestedforloop {
+    public static void main(String[] args) {
+        int i;
+
+        for (i=0;i<=20;i++){
+
+            if (i==6)
+                continue;
+            if (i==12)
+                continue;
+            if (i==17)
+                continue;
+                System.out.print(" "+i);
+
+
+
+
+        }
+
+    }
+}
