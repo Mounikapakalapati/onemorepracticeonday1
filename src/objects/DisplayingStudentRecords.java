3dd177a1e8a0e39e6e06edf3e2b1b5f725a2506e
mounika <mounikapakalapati09@gmail.com>
2024-06-17 16:17:45 -0500

    Student records

--- src/objects/DisplayingStudentRecords.java
+++ src/objects/DisplayingStudentRecords.java
@@ -30,3 +30,27 @@ public class DisplayingStudentRecords {
         s2.Modify(s2.name);
     }
 }
+
+ class Student {
+
+    // Displaying 10th standard records
+
+    int id;
+    String name;
+    int age;
+    double grade;
+
+    static String standard = "10th";
+
+    void Display(String content){
+
+        System.out.println(content);
+
+    }
+    void Modify(String content){
+        System.out.println(content);
+
+    }
+
+
+}
