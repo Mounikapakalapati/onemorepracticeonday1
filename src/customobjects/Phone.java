15658721e57f9a9b1929a18993b70dd9bb21a569
mounika <mounikapakalapati09@gmail.com>
2024-08-23 13:22:27 -0500

    Real time objects

---
+++ src/customobjects/Phone.java
@@ -0,0 +1,34 @@
+package customobjects;
+
+public class Phone {
+
+    Dimensions dms;
+    String color;
+    Screen screen;
+    Cameras camera;
+
+
+    void Call(){
+
+    }
+    void Browse(){
+
+    }
+    void TakingPictures(){
+
+    }
+    void LearnKnowledge(){
+
+    }
+
+
+}
+class Dimensions{
+
+}
+class Screen {
+
+}
+class Cameras {
+
+}
