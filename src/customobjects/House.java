00b1b8d8f9d721a84ee878d5dcd4bc8e2259c979
mounika <mounikapakalapati09@gmail.com>
2024-08-23 13:28:34 -0500

    Real time objects

---
+++ src/customobjects/House.java
@@ -0,0 +1,23 @@
+package customobjects;
+
+public class House {
+
+    int sqft;
+    String model;
+    MaterialDetails meterial;
+
+    void Tolive(){
+
+    }
+    void Shelter(){
+
+    }
+
+
+}
+class MaterialDetails{
+    String wood;
+    String Brick;
+    String color;
+
+}
